dfc7af15ced4f260740f351730848c84696352ed
Emanuelle <emanuellemoco>
2020-05-14 19:48:04 -0300

    atividade da aula 18

--- aula18.py
+++ aula18.py
@@ -11,10 +11,11 @@ dT = 5
 dX = 5
 
 t_list = [0,20,20,20,20,20,20,20,20,20,0]
+x = [0,5,10,15,20,25,30,35,40,45,50]
 #### 
 
 n = 0
-while(n<100):
+while(n<104):
     for i in range(len(t_list)):
         if i ==0:
             t = 0
@@ -22,7 +23,14 @@ while(n<100):
             t  = 0
         else:
             t =t_list[i] + alpha*dT/(dX**2)*(t_list[i+1]-2*t_list[i]+t_list[i-1])
+
+
         t_list[i] = t
+        
     n+=1
-    print(t_list)
+
+
+plt.plot(x,t_list)
+plt.grid()
+plt.show()
 
