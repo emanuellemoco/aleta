481ce465aefdb58852bf4988fa778a6cca151528
Lucas Leal Vale <lucaslealvale01@gmail.com>
2020-05-15 09:18:30 -0300

    fixed

--- aula18.py
+++ aula18.py
@@ -6,27 +6,24 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 
-alpha = 1 #cm2/s
-dT = 5
-dX = 5
+alpha = 0.0001  #m2/s
+dX = 0.05       #m
+dT = 5          #s
+
 
 t_list = [0,20,20,20,20,20,20,20,20,20,0]
-x = [0,5,10,15,20,25,30,35,40,45,50]
+x = [0,0.05,0.10,0.15,0.20,0.25,0.30,0.35,0.40,0.45,0.50]
 #### 
-
+beta=alpha*dT/(dX**2)
 n = 0
-while(n<104):
-    for i in range(len(t_list)):
-        if i ==0:
-            t = 0
-        elif (i==(len(t_list)-1)):
-            t  = 0
-        else:
-            t =t_list[i] + alpha*dT/(dX**2)*(t_list[i+1]-2*t_list[i]+t_list[i-1])
+aux = t_list[:]
+while(n<100):
 
+    for i in range(1,len(t_list)-1):
+        
+        aux[i] = t_list[i] + beta*(t_list[i+1]-2*t_list[i]+t_list[i-1])
 
-        t_list[i] = t
-
+    t_list = aux[:]
     n+=1
 
 print(t_list)
