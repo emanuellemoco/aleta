eb892495cfde63627c1699bd0984448e059de9e5
zMendes <leonardom1808@gmail.com>
2020-06-04 17:58:50 -0300

    UwU >.>

--- APS4/aps4.py
+++ APS4/aps4.py
@@ -11,20 +11,22 @@ def burger(m,n,T,t,dT,dX,dY, alpha, Q, K,pos_x,pos_y):
     C_temp = np.copy(C)
     q = Q/(dX*dY)
     print(q)
-    while(count-1<2):
+#ME DESMUTA AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA<-------
+    C_temp[pos_y][pos_x] +=q*dT
+    while(count-1<t/dT):
         for i in range(1,m-1):
             for j in range(1,n-1):
-                v = alpha * math.sin(math.pi*j*dX/5)
-                parte2 =  alpha*(C_temp[i][j+1]-C_temp[i][j-1])/(2*dX)
+                v = alpha * math.sin((math.pi/5)*j*dX)
+                #v = 0
+                parte2 =  alpha*((C_temp[i][j+1]-C_temp[i][j-1])/(2*dX))
                 parte3 =  v*(C_temp[i-1][j]-C_temp[i+1][j])/(2*dY)
                 parte4 = (C_temp[i][j+1]-2*C_temp[i][j]+C_temp[i][j-1])/(dX**2)
                 parte5 = (C_temp[i-1][j]-2*C_temp[i][j]+C_temp[i+1][j])/(dY**2)
                 if ((count*dT<=T) and i==pos_y and j==pos_x ):
-                    C[i][j] = C_temp[i][j] - dT * ( parte2 + parte3 - K * (parte4  + parte5  )- q )
+                    C[i][j] = C_temp[i][j] - dT * ( parte2 + parte3 - K * (parte4  + parte5  ) - q )
                 else:
                     #v = alpha * math.sin(math.pi*j*dX/5)
                     C[i][j] = C_temp[i][j] - dT*(parte2 + parte3 - K * (parte4 + parte5))
-                
                 
                 if(C[i][j]<0):
                     C[i][j] = 0
@@ -39,7 +41,7 @@ def burger(m,n,T,t,dT,dX,dY, alpha, Q, K,pos_x,pos_y):
 
         count+=1
         C_temp = np.copy(C)
-    print("Retornou depois de {0} segundos.".format(count*dT))
+    print("Retornou depois de {0} segundos.".format(int(count*dT)))
     return C
 K = 1      #m²/s
 alpha = 1   #m/s
@@ -47,8 +49,8 @@ T = 3       #s tempo despejo
 Q = 100     #kg/ms
 Lx = 30     #m
 Ly = 20     #m
-a =  0.5    #round(1/1.4)   #round(1/1.4,2)
-b = 60/6
+a = 1/1.4+1    #round(1/1.4,2)
+b = 60/(5+1)
 dX = 0.5  #
 dY = 0.5  #
 
@@ -59,14 +61,13 @@ index_b = int(b/dY)
 print(index_a,index_b)
 pontos_x = int((Lx / dX)) + 1
 pontos_y = int((Ly / dY)) + 1
-dT = ((1/(4*K)) * (dX * dY)) 
+dT = 0.01#((1/(4*K)) * (dX * dY))
 T_total = 10 * T  #tempo total
-
+print(dT)
 results = burger(pontos_y,pontos_x,T,T_total,dT,dX,dY,alpha,Q,K,index_a, index_b)
-#print(results[40][40])
 
-plt.imshow(results, cmap='jet', interpolation='nearest')
-plt.title("Resultado após duas iterações")
+plt.imshow(results, cmap='ocean', interpolation='nearest')
+plt.title("Resultado após {0} segundos".format(T_total))
 plt.gca().invert_yaxis()
 plt.colorbar()
 plt.show()
